a4223485ca7109e78c88c7c8c29235bf76eada55
Roger Lamb <eckorog2005@gmail.com>
2013-11-18 20:21:36 -0500

    basic layout for game gui

---
+++ src/cmsc519/team8/uno/gui/MainFrame.java
@@ -0,0 +1,48 @@
+package cmsc519.team8.uno.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+					frame.setTitle("UNO");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		//add menubar
+		UnoMenuBar menuBar = new UnoMenuBar();
+		setJMenuBar(menuBar);
+		
+	}
+
+}
---
+++ src/cmsc519/team8/uno/gui/UnoMenuBar.java
@@ -0,0 +1,27 @@
+package cmsc519.team8.uno.gui;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class UnoMenuBar extends JMenuBar {
+
+	/**
+	 * UID for JToolBar
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public UnoMenuBar() {
+		super();
+		
+		//add game option
+		JMenu menu = new JMenu("Game");
+		JMenuItem item = new JMenuItem("Help");
+		add(menu);
+		add(item);
+	}
+
+}
